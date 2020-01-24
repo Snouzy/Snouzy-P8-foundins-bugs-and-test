ec32991aac152b3e9b0fb27ce8158b409621706f
Mathias Bradiceanu <mathiasnouzy@gmail.com>
2020-01-24 13:35:00 +0100

    changing = .. by +=

--- js/template.js
+++ js/template.js
@@ -77,7 +77,7 @@
          template = template.replace('{{completed}}', completed);
          template = template.replace('{{checked}}', checked);
 
-         view = view + template;
+         view += template;
       }
 
       return view;
